d155779f6e480b193bfefd988e6811e560d0f5d6
Akhmadillo Mamirov <105142060+akhmadmamirov@users.noreply.github.com>
2023-03-09 20:23:46 -0500

    Update app.js

--- app.js
+++ app.js
@@ -14,12 +14,14 @@ app.get('/apikey', (req, res) => {
   // Replace this with your actual API key
   const apiKey = 'ecf82537022ea68bfe294599ccd9bfb2';
   const geoApi = 'AIzaSyBazbYas6WlmtCflXovhVkwXscJG3Kqd_w';
-  res.send({ apiKey, geoApi });
+  res.setHeader('Content-Type', 'application/json');
+  res.send(JSON.stringify({ apiKey, geoApi }));
 });
 
 // Start the server
-app.listen(3000, () => {
-  console.log('Server listening on port 3000');
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`Server listening on port ${port}`);
 });
 
 module.exports = app;
